608ca3682a51d1aab4a0f7c70bf2ff924356fa09
sonia1183 <80530004+sonia1183@users.noreply.github.com>
2021-10-28 10:45:37 +0530

    TO DO

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import ToDoList from './ToDoList';
 function App() {
   return (
     <div className="App">
+      <h1>TO DO LIST</h1>
       <header className="App-header">
         <ToDoList/>
         </header>
